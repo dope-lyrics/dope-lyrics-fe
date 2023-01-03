bb8bdd515185538d3432af88c9ff71777fd6697e
onursazak <onur.sazak@obss.com.tr>
2023-01-03 19:55:06 +0300

    Remove navbar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
   <RouterView></RouterView>
 </template>
 
