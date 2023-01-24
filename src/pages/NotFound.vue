186587bfa9360fbc77608fa49001c098b8d7efb5
onursazak <onur.sazak@obss.com.tr>
2023-01-24 19:47:21 +0300

    Add NotFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>We don't have this page yet.</div>
+  <RouterLink to="/">Go to homepage</RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang=""></style>
